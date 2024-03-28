889200ffa503d22e966a8fde840a35ba25d00613
AnnaBai95 <annastacia_bailey@yahoo.com>
2024-03-27 21:24:45 -0500

    Search box + Filter
    Increased the size of the search box and dynamically added the regions to the dropdown list

--- countries/src/App.js
+++ countries/src/App.js
@@ -6,6 +6,13 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import countryData from "./data/data.json";
 
 function App() {
+  // const handlePopulateFilter = () => {};
+
+  const uniqueRegions = countryData.filter(
+    (country, index, self) =>
+      (index === self.findIndex((c) => c.region === country.region))
+  );
+
   return (
     <div className="">
       <header className="px-10 py-5 bg-white">
@@ -19,26 +26,35 @@ function App() {
       </header>
       <main className="mt-10 px-10">
         <div className="flex justify-between">
-          <div className="input-group">
-            <FontAwesomeIcon icon={faSearch} className="icon" />
-            <input type="text" placeholder="Search for a country..."></input>
+          <div className="input-group w-4/12 inline-flex items-center">
+            <FontAwesomeIcon icon={faSearch} className="icon ps-1 pr-4" />
+            <input
+              type="text"
+              placeholder="Search for a country..."
+              className="w-full"
+            ></input>
           </div>
           <div>
             <select>
               <option>Filter by region</option>
+              {uniqueRegions &&
+                uniqueRegions.map((reg, index) => (
+                  <option key={index}>{reg.region}</option>
+                ))}
             </select>
           </div>
         </div>
         <div className="card-container flex flex-wrap gap-16 mt-10">
           {countryData &&
-            countryData.map((country) => (
+            countryData.map((country, index) => (
               <>
-                <div className="card bg-white rounded" key={country.area}>
+                <div className="card bg-white rounded" key={index}>
                   <div className="card-header">
                     <img
                       src={country.flags.png}
                       alt={`Flag of ${country.name}`}
-                    className="rounded-t w-full h-full"></img>
+                      className="rounded-t w-full h-full"
+                    ></img>
                   </div>
                   <div className="card-body p-7">
                     <p className="font-extrabold mb-3 title">{country.name}</p>
